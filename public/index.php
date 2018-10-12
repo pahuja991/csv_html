ef19fe5fb088ab1d9291785b9271d9836efd333b
pahuja991 <rp824@njit.edu>
2018-10-11 23:32:30 -0400

    Feature: Create classes name and features of recoreds and table

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
-    echo "hi";
-?>
+
+main::start("sales.csv");
+
+class main {
+
+    static public function start($filename) {
+        $records =csv::getRecords($filename); // reads csv file
+        $table = html::generateTable($records); // display table
+        system::displayTable($table);
+    }
+}
